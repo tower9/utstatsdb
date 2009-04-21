2712e665145bc7fa5090b22423043d629b886757
panther <panther@fbd5f4e5-e1bc-4abc-814b-7873bac8d5ba>
2009-04-21 21:00:13 +0000

    Table-based special events.

--- tables/mysql/playersgts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE %dbpre%playersgts (
-  gs_pnum mediumint(8) unsigned NOT NULL,
-  gs_stype smallint(5) unsigned NOT NULL,
-  gs_tnum smallint(5) unsigned NOT NULL,
-  gs_total mediumint(8) unsigned NOT NULL default 0,
-  KEY gs_ptn (gs_pnum,gs_stype,gs_tnum)
-) Type=MyISAM;
